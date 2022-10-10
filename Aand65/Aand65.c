697a05c64eeb2839281b2a24555e4d13795c6b1a
devjohnfifth <115366833+devjohnfifth@users.noreply.github.com>
2022-10-10 14:36:34 -0300

    And65

---
+++ Aand65/Aand65.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("if I put 65 in decimal form = %d\n",65);
+    printf("if I put 65 in caracter form = %c\n",65);
+    return 0;
+}
---
+++ Aand65/README.md
@@ -0,0 +1,2 @@
+I show the diference of 65 value in %d and %c.
+Thanks, João Quintão(John Fifth)
